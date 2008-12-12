9cf4b446a541c5b0b8d151f1a6fa5600e5513cb9
unknown <Administrator@.in.ibm.com>
2008-12-13 01:23:23 +0530

    Ankur 2008-12-12

---
+++ mustDo/apps/remindersList/modules/task/actions/actions.class.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * tasks actions.
+ *
+ * @package    mustDo
+ * @subpackage tasks
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class taskActions extends sfActions
+{
+	/**
+	 * Executes index action
+	 *
+	 * @param sfRequest $request A request object
+	 */
+	public function executeIndex(sfWebRequest $request)
+	{
+		$this->form = new TasksForm();
+		$userSession = $this->getUser();
+		//TODO remove this userid insertion this is just for test
+		if($userSession->getAttribute('userId') == null){
+			$userSession->setAttribute('userId', 1);
+		}
+		
+		if($request->isMethod('post')){
+			$this->form->bind($request->getParameter('task'));
+			if ($this->form->isValid())
+			{
+
+
+				$taskString = $request->getParameter('taskString');
+				$taskPriority = $this->form->getValue('taskPriority');
+				$taskDescription = $request->getParameter('taskDescription');
+				$active = $request->getParameter('active');
+
+				$task = new Tasks();
+				print_r($this->form->getValues() . "dsfs");
+				$task->setUserid($userSession->getAttribute('userId'));
+				if($this->form->getValue('active') == "on"){
+					$task->setActive(1);
+				}else{
+					$task->setActive(0);
+				}
+				
+				$task->setDeleted(0);
+				$task->setTaskdescription($this->form->getValue('taskDescription'));
+				$task->setTaskstring($this->form->getValue('taskString'));
+				$task->setTaskpriority(intval($taskPriority));
+				$task->save();
+				
+				//$task=>get
+
+				$params = array(
+    				'taskString'    => $request->getParameter('taskString'),
+    				'taskPriority'   => $request->getParameter('taskPriority'),
+    				'taskDescription' => $request->getParameter('taskDescription'),
+  					'active' => $request->getParameter('active')
+				);
+
+				$this->redirect('tasks/thankyou?'.http_build_query($this->form->getValues()));
+			}
+		}
+	}
+
+	public function executeThankyou($request)
+	{
+	}
+
+
+}
---
+++ mustDo/lib/form/TasksForm.class.php
@@ -0,0 +1,38 @@
+<?php 
+class TasksForm extends sfForm
+{
+  protected static $taskPriority = array('1', '2', '3', '4', '5');
+  
+  public function configure()
+  {
+    $this->setWidgets(array(
+      'taskString'    => new sfWidgetFormInput(),
+      'taskPriority' => new sfWidgetFormSelect(array('choices' => self::$taskPriority)),
+      'taskDescription' => new sfWidgetFormTextarea(),
+      'active' => new sfWidgetFormInputCheckbox()
+    ));
+    
+    $this->widgetSchema->setLabels(array(
+ 		'taskString'    => 'Task name',
+  		'taskPriority'   => 'Task Priority',
+  		'taskDescription' => 'Task Description',
+  		'active' => "Active"
+	));
+	
+	$this->setDefault('taskPriority', '2');
+	$this->setDefault('active', 'checked');
+	
+	$this->widgetSchema->setNameFormat('task[%s]');
+	
+	
+	$this->setValidators(array(
+      'taskString'    => new sfValidatorString(array('required' => true, 'max_length' => 400)),
+      'taskPriority' => new sfValidatorChoice(array('choices' => array_keys(self::$taskPriority))),
+      'taskDescription' => new sfValidatorString(array('max_length' => 1000)),
+	  'active' => new sfValidatorString(array('required' => false))
+    ));
+	
+	
+    
+  }
+}
